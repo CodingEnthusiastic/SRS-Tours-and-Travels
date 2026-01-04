b82319984b83395f3673744a5ef8da06d60e65c0
CodingEnthusiastic <rishabh10d58@gmail.com>
2026-01-04 18:14:01 +0530

    Fix static files serving and enhance navbar styling with hover effects

--- sample/settings.py
+++ sample/settings.py
@@ -132,5 +132,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'sample/static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
